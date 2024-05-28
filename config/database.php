06716370430b3728121c20d6d6bd7d64bf75e63d
Baobab_codeur <jeanamekpod@gmail.com>
2024-05-28 17:32:36 +0000

    mon premier commit

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$server_name = 'localhost';
+$username = 'root';
+$password = "";
+
+
+try {
+    $conn = new PDO("mysql:host=$server_name;dbname=dsend", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
